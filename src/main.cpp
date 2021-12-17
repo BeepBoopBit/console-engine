2c7f49ea159d1c8c480cc87d8cc1deca68102d48
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2021-12-17 18:37:55 +0800

    COLOR HAS SUCCESSFULLY IMPLEMENTED

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,28 @@ void sleepFor(int x){
     std::this_thread::sleep_for(timespan);
 }
 
+CE::RigidBody *createNewTrees(int x, int y, char chr,  CE::ConsoleScreen *screen){
+    return new CE::RigidBody(screen,chr,x,y);
+}
+
 int main(){
-    CE::ConsoleScreen mainScreen(5,5,5,5,'#');
-    CE::RigidBody Tree(&mainScreen,'T',1,1);
-    CE::RigidBody Stone(&mainScreen,'O',1,2);
+    CE::ConsoleScreen mainScreen(10,10,5,5,'#');
+    CE::ConsoleEnvironmentHandler _handle;
+    for(int i = 0; i < 6; ++i){ // top
+        _handle.add(createNewTrees(i+2,1,'P',&mainScreen));
+    }
+    for(int i = 0; i < 6; ++i){ // left
+        _handle.add(createNewTrees(2,2+i,'P',&mainScreen));
+    }
+    for(int i = 0; i < 6; ++i){ // right
+        _handle.add(createNewTrees(7,2+i,'P',&mainScreen));
+    }
+    for(int i = 0; i < 6; ++i){ // bottom
+        _handle.add(createNewTrees(i+2,8,'P',&mainScreen));
+    }
+
+    _handle.setColor(100,50,120);
     mainScreen.print();
+
 
 }
