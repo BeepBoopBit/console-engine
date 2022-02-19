9ebc4a67c1421027f50fae98d5dc8b3c19957409
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-02-19 15:02:08 +0800

    Added AbstractConsoleEntity and ConsoleEntityHandler

--- include/consoleEntity.h
+++ include/consoleEntity.h
@@ -6,14 +6,22 @@
 
 
 namespace CE{
-    class ConsoleEntity{
+    class AbstractConsoleEntity{
+    public: // move function
+        virtual void move(int pX, int pY, int nX, int nY) = 0;
+        virtual void moveUp(int num) = 0;
+        virtual void moveLeft(int num) = 0;
+        virtual void moveDown(int num) = 0;
+        virtual void moveRight(int num) = 0;
+    };
+    class ConsoleEntity : AbstractConsoleEntity{
     public:
         typedef std::string::size_type sType;
     public: // constructor
         ConsoleEntity(Observer *observer, char character, int x = 0, int y = 0): _observer(observer), _character(character), _pX(x), _pY(y), _cX(x), _cY(y){
             _observer->notifyInitialPosition(x,y,character);
         }
-    public:
+    public: // movement
         void move(int pX, int pY, int nX, int nY){
             _pX = pX;
             _pY = pY;
@@ -33,11 +41,66 @@ namespace CE{
         void moveRight(int num){
             move(_cX, _cY, _cX+num, _cY);
         }
+    
+    public: // getters
+        sType getPreviousX(){
+            return _pX;
+        }
+        sType getPreviousY(){
+            return _pY;
+        }
+        sType getCurrentX(){
+            return _cX;
+        }
+        sType getCurrentY(){
+            return _cY;
+        }
     private:
         sType _pX, _pY;
         sType _cX, _cY;
         CE::Observer *_observer;
         char _character;
+    };
+
+    // handles bunch of ConsoleEntity (TO BE IMPLEMENTED);
+    class ConsoleEntityHandler : AbstractConsoleEntity{
+    public: // utility
+        void attach(ConsoleEntity *entity){
+            _entities.push_back(entity);
+        }
+    public: // ConsoleEntity Stuffs
+        void move(int pX, int pY, int nX, int nY){
+            auto entitySize = _entities.size();
+            for(int i = 0; i < entitySize; ++i){
+                _entities[i]->move(pX,pY,nX,nY);
+            }
+        }
+        void moveUp(int num){
+            auto entitySize = _entities.size();
+            for(int i = 0; i < entitySize; ++i){
+                _entities[i]->moveUp(num);
+            }
+        }
+        void moveLeft(int num){
+            auto entitySize = _entities.size();
+            for(int i = 0; i < entitySize; ++i){
+                _entities[i]->moveLeft(num);
+            }
+        }
+        void moveDown(int num){
+            auto entitySize = _entities.size();
+            for(int i = 0; i < entitySize; ++i){
+                _entities[i]->moveDown(num);
+            }
+        }
+        void moveRight(int num){
+            auto entitySize = _entities.size();
+            for(int i = 0; i < entitySize; ++i){
+                _entities[i]->moveRight(num);
+            }
+        }
+    private:
+        std::vector<ConsoleEntity*> _entities;
     };
 }
 #endif // CONSOLE_ENTITY_H
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,16 @@
 int main(){
     CE::ConsoleScreen *mainScreen = new CE::ConsoleScreen(10,10,'#');
     CE::Observer *mainObserver = new CE::Observer(mainScreen);
-    CE::ConsoleEntity SnakeHead(mainObserver,'P');
-    SnakeHead.moveDown(3);
+    CE::ConsoleEntity *SnakeHead = new CE::ConsoleEntity(mainObserver,'P', 5, 0);
+    CE::ConsoleEntity *SnakeBody00 = new CE::ConsoleEntity(mainObserver,'O', 4,0);
+    CE::ConsoleEntity *SnakeBody01 = new CE::ConsoleEntity(mainObserver,'O', 3,0);
+    CE::ConsoleEntityHandler WholeSnake;
+    WholeSnake.attach(SnakeHead);
+    WholeSnake.attach(SnakeBody00);
+    WholeSnake.attach(SnakeBody01);
+    mainScreen->print();
+    WholeSnake.moveDown(2);
+    std::cout << std::endl;
     mainScreen->print();
 
     
